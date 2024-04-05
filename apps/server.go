c3454e54eaac479e79e301e94321bcca18af7295
SamuelAndreass <rudibego775@gmail.com>
2024-04-05 08:29:09 +0700

    .

--- apps/server.go
+++ apps/server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net"
+	"time"
 
 	"github.com/SamuelAndreass/sampleproject/handle"
 )
 
 func main() {
+	// membuat koneksi baru paada localhost / ip kita dan pada port 1234
 	net, err := net.Listen("tcp", "localhost:1234")
 	if err != nil {
 		print(err)
@@ -14,16 +16,20 @@ func main() {
 	}
 	handle.HandleErr(err)
 	defer net.Close()
+
+	// menghandle pesan dari client 
+	// for loop infinit bertujuan agar bisa menerima beberapa pesan dari client
 	for {
 		conn, err := net.Accept()
 		if err != nil {
 			print(err)
 			return
 		}
+	// go routine untuk membaca pesan
 		go handleServer(conn)
 	}
 
-}tjthtthththth
+}
 
 func handleServer(conn net.Conn) {
 	defer conn.Close()
@@ -34,6 +40,19 @@ func handleServer(conn net.Conn) {
 		return
 	}
 
-	print("pesan dari client:", string(buffer[:n]))
+	// setwritedeadline ditujuan ketika pesan gagal terkirim dengan rentang waktu 5 detik
+	// dari waktu sekarang / ketika pesan coba untuk dikirim / write
+	err = conn.SetWriteDeadline(time.Now().Add(time.Second * 5))
+	if err != nil {
+		if netErr, ok := err.(net.Error); netErr.Timeout() && ok {
+			print("jaringan terputus")
+		}
+		handle.HandleErr(err)
+
+	}
+	// setelah pesan diterima dari client akan dikirim kembali ke client
+	print("pesan dari client: ", string(buffer[:n]))
+	_, err = conn.Write(buffer[:n])
+	handle.HandleErr(err)
 
 }
--- test/client.go
+++ test/client.go
@@ -3,16 +3,39 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 
 	"github.com/SamuelAndreass/sampleproject/handle"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:1234")
+	// untuk menkoneksikan jaringan ke server dengan local ip di port 1234
+	// sama seperti net.Dial, net.DialTimeout menerima timeout artinya gagal terhubung
+	// pada codingan ini menerima timeout ketika sudah 5 detik 
+	// dan tidak terkoneksi jaringan manapun
+	conn, err := net.DialTimeout("tcp", "localhost:1234", time.Second * 5)
 	handle.HandleErr(err)
 	defer conn.Close()
 	fmt.Println("koneksi tertutup")
 
+	// mengrim pesan ke server dalam bentuk array of byte
 	conn.Write([]byte("Hello server"))
+
+	// SetReadDeadline ditujuan ketika pesan gagal diterima dengan rentang waktu 5 detik
+	// dari waktu sekarang / ketika pesan coba untuk dibaca / read akan menerima eror timeout
+	err = conn.SetReadDeadline(time.Now().Add(time.Second* 5))
+	if err != nil {
+		if netErr, ok :=  err.(net.Error); netErr.Timeout() && ok {
+			print("jaringan terputus")
+		} 
+		handle.HandleErr(err)
+	}
+	// menampung pesan dengan buffer dan print hasil pesan tersebut
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+
+	handle.HandleErr(err)
+	print("pesan balasan dari server: ", string(buffer[:n]))
+
 
 }
